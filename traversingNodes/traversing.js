4e787ca1036825784fc2205dc960641b8f790f4e
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-06-30 09:13:23 -0500

    Doing some cardio.

---
+++ traversingNodes/traversing.js
@@ -0,0 +1,6 @@
+console.log("Let's traverse some nodes!");
+
+//First of all, what's the difference between an ELEMENT and a NODE?
+var ryan = document.querySelector(`#ryan`)
+console.log(ryan.children);
+console.log(ryan.childNodes);
